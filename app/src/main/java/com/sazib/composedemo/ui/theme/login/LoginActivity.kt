581374c7a6b57d7351222adb8ab54ef516bfc032
sazib <sazib.islam@btracsl.com>
2023-01-30 11:55:28 +0600

    login

--- app/src/main/java/com/sazib/composedemo/ui/theme/login/LoginActivity.kt
+++ app/src/main/java/com/sazib/composedemo/ui/theme/login/LoginActivity.kt
@@ -8,9 +8,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sazib.composedemo.ui.theme.White
@@ -34,8 +44,22 @@ class LoginActivity : ComponentActivity() {
         .background(color = White)
         .verticalScroll(rememberScrollState())
     ) {
+      var username by remember { mutableStateOf("") }
+      var password by rememberSaveable { mutableStateOf("") }
 
+      OutlinedTextField(
+        value = username,
+        onValueChange = { username = it },
+        label = { Text("Email") }
+      )
 
+      OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+      )
 
     }
   }
---
+++ app/src/main/java/com/sazib/composedemo/ui/theme/login/SignUpViewModel.kt
@@ -0,0 +1,16 @@
+package com.sazib.composedemo.ui.theme.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+
+  var username by mutableStateOf("")
+    private set
+
+  fun updateUsername(input: String) {
+    username = input
+  }
+}
