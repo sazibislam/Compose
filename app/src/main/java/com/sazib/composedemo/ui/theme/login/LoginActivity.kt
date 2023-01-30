90f00a535632e4073d6f1b52b1facac4d2471e44
sazib <sazib.islam@btracsl.com>
2023-01-30 13:29:50 +0600

    Login button

--- app/src/main/java/com/sazib/composedemo/ui/theme/login/LoginActivity.kt
+++ app/src/main/java/com/sazib/composedemo/ui/theme/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.sazib.composedemo.ui.theme.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +13,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -20,11 +25,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.sazib.composedemo.ui.theme.dashboard.MainActivity
+import com.sazib.composedemo.R
 import com.sazib.composedemo.ui.theme.White
 
 class LoginActivity : ComponentActivity() {
@@ -52,6 +62,8 @@ class LoginActivity : ComponentActivity() {
       horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+      LogoImage()
+      Spacer(modifier = Modifier.height(20.dp))
       Text(text = "Please login")
       Spacer(modifier = Modifier.height(20.dp))
 
@@ -75,13 +87,38 @@ class LoginActivity : ComponentActivity() {
 
       Button(
         onClick = {
-          viewModel.login()
+
+          startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+
+          // viewModel.login()
         },
         modifier = Modifier
           .padding(8.dp)
       ) {
         Text(text = "Login")
       }
+    }
+  }
+
+  @Composable
+  fun LogoImage() {
+
+    Column(
+      modifier = Modifier
+        .wrapContentWidth()
+        .wrapContentHeight(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+      Image(
+        painterResource(R.drawable.ic_logo),
+        contentDescription = "",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+          .height(height = Dp(60F))
+          .width(width = Dp(60F))
+      )
     }
   }
 }
